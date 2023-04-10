be01a8cc7614f5fbec2a13ad0c5f1b63c946e85a
importantsgit <dlwognsdk@tukorea.ac.kr>
2023-04-10 16:48:53 +0900

    Initial Commit

---
+++ ObjectDetectionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjectDetectionApp
+//
+//  Created by 이재훈 on 2023/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
