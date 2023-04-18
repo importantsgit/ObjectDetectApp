af027956a2d2e846526c0fc66ccbb2254cbefd44
importantsgit <dlwognsdk@tukorea.ac.kr>
2023-04-18 17:46:28 +0900

    add videoView control

--- ObjectDetectionApp/View/Tabbar/Items/Video/VideoViewController.swift
+++ ObjectDetectionApp/View/Tabbar/Items/Video/VideoViewController.swift
@@ -7,8 +7,21 @@
 
 import UIKit
 import SnapKit
+import PhotosUI
 
 class VideoViewController: OBViewController {
+    
+    private lazy var picker: PHPickerViewController = {
+        var pickerConfiguration = PHPickerConfiguration()
+        pickerConfiguration.filter = .any(of: [.videos])
+        pickerConfiguration.selectionLimit = 1
+        pickerConfiguration.selection = .ordered
+        
+        let picker = PHPickerViewController(configuration: pickerConfiguration)
+        picker.delegate = self
+        
+        return picker
+    }()
     
     var titleView: UIView = {
         let uiView = UIView()
@@ -24,6 +37,16 @@ class VideoViewController: OBViewController {
         return label
     }()
     
+    private lazy var pickerButton: VideoButton = {
+        let button = VideoButton()
+        button.setupLayout(radius: 32.0, backColor: .blue)
+        button.putImage(image: UIImage(systemName: "square.and.arrow.down") ?? UIImage(), backColor: .blue)
+        
+        button.addTarget(self, action: #selector(pickerButtonTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
     private lazy var button: VideoButton = {
         var button = VideoButton()
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
@@ -32,7 +55,7 @@ class VideoViewController: OBViewController {
         return button
     }()
     
-    var containView = StreamingView()
+    var containView = VideoView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,7 +76,7 @@ class VideoViewController: OBViewController {
 
 extension VideoViewController {
     private func setupLayout() {
-        [containView, titleView, button].forEach{
+        [containView, titleView, button, pickerButton].forEach{
             self.view.addSubview($0)
         }
         
@@ -66,6 +89,11 @@ extension VideoViewController {
         button.snp.makeConstraints{
             $0.width.height.equalTo(64)
             $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(32)
+        }
+        pickerButton.snp.makeConstraints{
+            $0.width.height.equalTo(64)
+            $0.left.equalToSuperview().inset(24)
             $0.bottom.equalToSuperview().inset(32)
         }
         
@@ -87,10 +115,32 @@ extension VideoViewController {
         
         if button.isSelected {
             button.isSelected = false
-            containView.stopCaptureSession()
+            containView.play()
         } else {
             button.isSelected = true
-            containView.startCaptureSession()
+            containView.pause()
+        }
+    }
+    
+    @objc func pickerButtonTapped() {
+        self.present(picker, animated: true, completion: nil)
+    }
+}
+
+
+extension VideoViewController: PHPickerViewControllerDelegate{
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        let itemProvider = results.first?.itemProvider
+        itemProvider?.loadFileRepresentation(forTypeIdentifier: UTType.movie.identifier){url, error in
+            if let url = url {
+                
+                DispatchQueue.main.sync { [weak self] in
+                    guard let self = self else {return}
+                    self.picker.dismiss(animated: true)
+                    self.containView.setURL(url: url)
+                }
+            }
         }
     }
 }
